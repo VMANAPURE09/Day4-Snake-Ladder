cefe9012a1445748c3889d91bff16ba8e05de2b0
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-25 13:29:42 +0530

    UC4 Reach Winning Postion

--- Snake-Ladder/Snake-Ladder/Program.cs
+++ Snake-Ladder/Snake-Ladder/Program.cs
@@ -16,20 +16,30 @@ namespace Snake_Ladder
 
             int option = random.Next(0, 3);
 
-            switch (option)
+            while (position <= 100)
             {
-                case 0:
-                    Console.WriteLine("No play");
-                    break;
-                case 1:
-                    Console.WriteLine("Ladder");
-                    position = position + number;
-                    break;
-                case 2:
-                    Console.WriteLine("Snake");
-                    position = position - number;
-                    break;
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No play");
+                        break;
+                    case 1:
+                        Console.WriteLine("Ladder");
+                        position = position + number;
+                        break;
+                    case 2:
+                        Console.WriteLine("Snake");
+                        position = position - number;
+                        break;
+
+
+                }
+                if (position < 0)
+                {
+                    position = 0;
+                }
             }
         }
+        
     }
 }
