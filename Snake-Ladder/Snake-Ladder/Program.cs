1953f23614b5d37089456d1fd251b7ead6e8f764
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-25 12:52:39 +0530

    UC1-SinglePlayer

---
+++ Snake-Ladder/Snake-Ladder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snake_Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            int position = 0;
+            Console.WriteLine("Position is " + position);
+        }
+    }
+}
